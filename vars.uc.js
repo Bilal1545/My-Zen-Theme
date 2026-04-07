a743854a7a190ecec9397f86de692355b35e6f63
Bilal1545 <abacar1243@gmail.com>
2026-04-07 22:58:22 +0300

    create

---
+++ vars.uc.js
@@ -0,0 +1,4 @@
+user_pref("widget.transparent-windows", true);
+user_pref("browser.tabs.allow_transparent_browser", true);
+user_pref("zen.widget.linux.transparency", true);
+user_pref("zen.view.grey-out-inactive-windows", false);
