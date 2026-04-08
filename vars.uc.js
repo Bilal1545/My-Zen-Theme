c7ab9f7df0f1f7f07999b4a0563c03842c4568ff
Bilal1545 <abacar1243@gmail.com>
2026-04-08 16:09:57 +0300

    a

--- vars.uc.js
+++ vars.uc.js
@@ -2,3 +2,5 @@ user_pref("widget.transparent-windows", true);
 user_pref("browser.tabs.allow_transparent_browser", true);
 user_pref("zen.widget.linux.transparency", true);
 user_pref("zen.view.grey-out-inactive-windows", false);
+user_pref("browser.display.background_color", "#ffffff33");
+user_pref("browser.display.background_color.dark", "#ffffff09");
